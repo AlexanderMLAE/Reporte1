b4229641e3654a37b8a9b966d14f386f66c100dc
AlexanderMLAE <minevenger@gmail.com>
2025-10-06 22:24:14 -0500

    Primer progreso

--- dictReader.py
+++ dictReader.py
@@ -1,14 +1,34 @@
+import csv
+
 # alr how we gon do this
 # nested dicts?
 # one dictionary with each course as a key and another dictionary as a value
 # all data per specific course will be on the nested dict
 # sounds good?
 
-exDict = { # nombre_curso, creditos, id_prerrequisitos
-    "id_curso" : 1,
-    "nombre_curso" : "nombre",
-    "creditos" : 0,
-    "id_prerequisitos": 0,
-}
 
-print(exDict)
+def readFile(): 
+
+    counter = 0
+
+    with open('cursos.csv', newline='') as csvfile:
+
+        reader = csv.DictReader(csvfile)
+
+        for row in reader:
+
+            print(row)
+            counter += 1
+    return counter
+
+# Find total amount
+
+def findTotal():
+
+    total = readFile()
+
+    print("Total of courses: ", total)
+
+findTotal()
+
+
