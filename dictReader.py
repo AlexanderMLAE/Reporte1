4f010ed7fc2c74ae81c70b2297b34c1ef03967d9
AlexanderMLAE <minevenger@gmail.com>
2025-10-06 21:57:57 -0500

    si

---
+++ dictReader.py
@@ -0,0 +1,14 @@
+# alr how we gon do this
+# nested dicts?
+# one dictionary with each course as a key and another dictionary as a value
+# all data per specific course will be on the nested dict
+# sounds good?
+
+exDict = { # nombre_curso, creditos, id_prerrequisitos
+    "id_curso" : 1,
+    "nombre_curso" : "nombre",
+    "creditos" : 0,
+    "id_prerequisitos": 0,
+}
+
+print(exDict)
